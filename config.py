4f84cb7b7876c852dad348eea5291bd9c6bb5c93
Lee Quan Jin <130299519+leequanjin@users.noreply.github.com>
2025-08-24 16:29:53 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.c8wid7cknqaa.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "leequanjin-employee"
+customregion = "us-east-1"
+
